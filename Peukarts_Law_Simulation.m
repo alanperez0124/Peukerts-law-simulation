7d30b2e4e0bb16491e025491054456f7343bbbb5
Alan Perez <perezalan012400@gmail.com>
2022-05-26 14:29:46 -0700

    Initial Commit; added basic comments to construct framework of project

---
+++ Peukarts_Law_Simulation.m
@@ -0,0 +1,23 @@
+function[] = Peukarts_Law_Simulation( )
+% This main funcion will load in data from a CSV file and will simulate 
+% Peukart's law. 
+
+
+    % Variables
+    % 
+    
+    % Initialize Peukert's Constant 
+    
+    
+    % Load in the data
+    
+    
+    % Create hypothetical discharge currents between 0 and 100
+    
+    
+    
+    % Plot x = Amps and y = time
+    
+    
+    
+    
