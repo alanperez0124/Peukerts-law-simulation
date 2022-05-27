fe950fd275b91085266436c03b0794ce99624804
alan perez <perezalan012400@gmail.com>
2022-05-27 13:01:53 -0700

    Basic exploration of peukerts law; Created log-log plot for hypothetical discharge currents between 0 and 100 amps

--- Peukarts_Law_Simulation.m
+++ Peukarts_Law_Simulation.m
@@ -4,20 +4,43 @@ function[] = Peukarts_Law_Simulation( )
 
 
     % Variables
-    % 
+    %  k        Peukert's constant used in the equation
+    %  f        Peukert's function 
     
     % Initialize Peukert's Constant 
-    
-    
-    % Load in the data
+    k = 1.249; 
+    coeff = 22263; 
+
+    % Create function
+    % Input: 
+    %       c The constant coefficient in front
+    %       k Peukert's constant
+    %       x The amps passed into the function
+    f = @(c, k, x) c*x.^(-k); 
     
     
     % Create hypothetical discharge currents between 0 and 100
-    
-    
-    
+    afDischarges = [1 : 4 : 100];
+       
+    % Get times at discharge values
+    afTimes = f(coeff, k, afDischarges); 
+
     % Plot x = Amps and y = time
+    22263*(11.5)^(-1.249)
+    %plot(log(afDischarges), log(afTimes), 'bo')
     
+    % Goals: 
+    % - Learn how to calculate Peukert's constant using existing data
+    % - Roughly understand how the batteries differ from each other. 
+
+    % Longterm goals: 
+    % - Implement feature that lets user input csv file with columns of
+    % batteries, asks them for a time (115 minutes) and outputs the
+    % discharge rate (current) in amps. 
+    % - 
+    
+    
+
     
     
     
